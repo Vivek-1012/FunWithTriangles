48f6e2919082661805b7544734d4a776fbcfcf93
Vivek-1012 <110529479+Vivek-1012@users.noreply.github.com>
2022-09-03 00:27:43 +0530

    doc

---
+++ hypoteneous.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#Hypotenuse-btn");
+const output = document.querySelector("#output");
+
+function calculateSumOfSquares(a,b) {
+    var sumOfSquare = a*a + b*b;
+    return sumOfSquare;
+} 
+
+function calhypo(){
+     const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+     const lengthOfhypoteneuse = Math.sqrt(sumOfSquares);
+     console.log(lengthOfhypoteneuse);
+}
+
+hypotenuseBtn.addEventListener("click", calhypo);
