5dbf92549faf7d7b81a0ccc5c0dd24ae7658a73f
Vivek-1012 <110529479+Vivek-1012@users.noreply.github.com>
2022-09-03 16:15:03 +0530

    doc

--- area.js
+++ area.js
@@ -1,6 +1,6 @@
-const inputSides = document.querySelectorAll(".input")
-const checkBtn = document.querySelector("#CheckArea")
-const outPut = document.queryCommandValue("#outputA")
+const inputSides = document.querySelectorAll(".input");
+const checkBtn = document.querySelector("#CheckArea");
+const outPut = document.querySelector("#outputA");
 
 function calculateArea(base , height) {
     const calculation = base * height /2 ;
@@ -10,10 +10,9 @@ function calculateArea(base , height) {
 
 function triangleIs() {
     const calculation = calculateArea(Number(inputSides[0].value),Number(inputSides[1].value));
-    if(calculation > 0 ) {
-    output.innerText = "Area of Triangle is " + calculation + "cm²";} else{
-        outPut.innerText = "Enter the correct value";
+    outPut.innerText = "Area of Triangle is " + calculation + " cm² ";
+    
     }
-}
+
 
 checkBtn.addEventListener("click", triangleIs);
